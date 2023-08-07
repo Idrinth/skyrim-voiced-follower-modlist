2d5b3b0d0017998ccb1872cb2697c350cc4f4ec9
Björn Büttner <Idrinth@users.noreply.github.com>
2023-08-07 20:10:30 +0200

    add sorting

--- bin/build.js
+++ bin/build.js
@@ -11,5 +11,20 @@ handlebars.registerHelper('lowerCase', function (string) {
 });
 fs.mkdirSync(__dirname + '/../deploy');
 yaml.parseFile('mods.yml', function (mods) {
+    mods.sort((a, b) => {
+        if (a.updated > b.updated) {
+            return 1;
+        }
+        if (a.updated < b.updated) {
+            return -1;
+        }
+        if (a.publisched < b.published) {
+            return 1;
+        }
+        if (a.published > b.published) {
+            return -1;
+        }
+        return 0;
+    });
     fs.writeFileSync(__dirname + '/../deploy/index.html', handlebars.compile(fs.readFileSync(__dirname + '/template.html', 'utf8'))({mods}));
 });
