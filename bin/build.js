73369eb20b328f9cc2a64d1a8a465b33cb038be0
Björn Büttner <Idrinth@users.noreply.github.com>
2023-11-04 22:01:39 +0100

    splitting the css data adding missing lang to template html adjusting build.js

--- bin/build.js
+++ bin/build.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 const yaml = require('yamljs');
 const handlebars = require('handlebars');
+const crypto = require('crypto');
+
+const fileHash = (file) => {
+    const hash = crypto.createHash('md5');
+    hash.setEncoding('hex');
+    hash.write(fs.readSync(__dirname + '/../styles/' + file, 'utf8'));
+    hash.end();
+    return hash.read();
+}
 handlebars.registerHelper('dateOnly', function (date) {
     const dt = new Date(date);
     return `${dt.getFullYear()}-${dt.getMonth() + 1}-${dt.getDate()}`;
@@ -13,6 +22,12 @@ handlebars.registerHelper('lowerCase', function (string) {
     return `${string}`.toLowerCase();
 });
 fs.mkdirSync(__dirname + '/../deploy');
+for (const style in ['design.css', 'reset.css', 'layout.css']) {
+    fs.writeFileSync(
+        __dirname + '/../deploy/' + style,
+        fs.readFileSync(__dirname + '/../styles/' + style, 'utf8')
+    );
+}
 yaml.parseFile('mods.yml', function (mods) {
     mods = mods.sort((a, b) => {
         if (a.updated.getTime() > b.updated.getTime()) {
@@ -29,5 +44,13 @@ yaml.parseFile('mods.yml', function (mods) {
         }
         return 0;
     });
-    fs.writeFileSync(__dirname + '/../deploy/index.html', handlebars.compile(fs.readFileSync(__dirname + '/template.html', 'utf8'))({mods}));
+    fs.writeFileSync(
+        __dirname + '/../deploy/index.html',
+        handlebars.compile(fs.readFileSync(__dirname + '/template.html', 'utf8'))({
+            mods,
+            layoutHash: fileHash('layout.css'),
+            resetHash: fileHash('reset.css'),
+            designHash: fileHash('design.css')
+        })
+    );
 });
