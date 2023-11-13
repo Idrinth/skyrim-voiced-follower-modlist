3eaf00f61726f46584b7b8d45c2d10aa34d49a9e
Björn Büttner <Idrinth@users.noreply.github.com>
2023-11-13 07:11:35 +0100

    make titles work

--- resources/scripts/title.js
+++ resources/scripts/title.js
@@ -1,4 +1,14 @@
 (() => {
+    const calcOffset = (element) => {
+        let top = 0;
+        let left = 0;
+        do {
+            top += element.offsetTop || 0;
+            left += element.offsetLeft || 0;
+            element = element.offsetParent;
+        } while(element);
+        return {top, left};
+    }
     const els = document.getElementsByClassName('title');
     for (let i=0;i< els.length;i++) {
         const element = els.item(i);
@@ -7,9 +17,13 @@
         });
         element.previousElementSibling.addEventListener('mouseover', () => {
             element.classList.toggle('active');
+            const offset = calcOffset(element.previousElementSibling);
+            element.setAttribute('style', 'left:'+offset.left+'px; top:'+offset.top+'px');
         });
         element.previousElementSibling.addEventListener('click', () => {
             element.classList.toggle('active');
+            const offset = calcOffset(element.previousElementSibling);
+            element.setAttribute('style', 'left:'+offset.left+'px; top:'+offset.top+'px');
         });
     }
 })();
