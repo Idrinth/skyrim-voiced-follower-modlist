56ca326fb8a6287840dac97fdaed39b8549e561b
Björn Büttner <Idrinth@users.noreply.github.com>
2023-11-13 06:59:27 +0100

    make titles work

--- resources/scripts/title.js
+++ resources/scripts/title.js
@@ -1,3 +1,15 @@
 (() => {
-
+    const els = document.getElementsByClassName('title');
+    for (let i=0;i< els.length;i++) {
+        const element = els.item(i);
+        element.addEventListener('mouseout', () => {
+            element.classList.toggle('active');
+        });
+        element.previousElementSibling.addEventListener('mouseover', () => {
+            element.classList.toggle('active');
+        });
+        element.previousElementSibling.addEventListener('click', () => {
+            element.classList.toggle('active');
+        });
+    }
 })();
